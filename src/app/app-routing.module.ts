d43c37226871d3c5552e4b76c09e6d5f88c566f5
mahyarniknaddaf <mahyarniknaddaf@gmail.com>
2023-10-08 20:27:38 +0330

    new edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PagesComponent ,title:"Pages"},
+  { path: 'notfound', component: NotfoundComponent ,title:"NotFoundPage"},
+  { path: '**', redirectTo: 'notfound' },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
